a5fba315324b7ddb05f4e1738f3102978f64176f
Imran Kabir <imrankabir325@gmail.com>
2025-01-20 21:18:31 +0600

    Valid parentheses using stack

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,14 +1,32 @@
 class Solution:
-    def isValid(self, s):
+    def bruteForce(self, s):
         while '()' in s or '{}' in s or '[]' in s:
             s = s.replace('()', '')
             s = s.replace('{}', '')
             s = s.replace('[]', '')
         return s == ''
     
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 == 1:
+            return False
+        stack = []
+        for i in s:
+            if i == '(' or i == '{' or i == '[':
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                else:
+                    if i == ')' and stack[-1] == '(':
+                        stack.pop()
+                    elif i == '}' and stack[-1] == '{':
+                        stack.pop()
+                    if i == ']' and stack[-1] == '[':
+                        stack.pop()
+        return len(stack) == 0        
 sol = Solution()
-# s = "([{}])"
-s = "[(])"
+s = "([{}])"
+# s = "[(])"
 
 print(f"Input: s = {s}")
 print(f"Output: {sol.isValid(s)}")
